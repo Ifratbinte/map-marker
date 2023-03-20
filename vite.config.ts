c5b9b96ec456d2bba73e7f8bed6e04454139bd67
Ifrat Binte <ifratbinte@gmail.com>
2023-03-20 10:52:28 +0600

    configure alias on config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "#assets": resolve(__dirname, "./src/assets/"),
+      "#components": resolve(__dirname, "./src/components/"),
+      "#routes": resolve(__dirname, "./src/routes/"),
+      "#store": resolve(__dirname, "./src/store/"),
+    },
+  },
   plugins: [react()],
 })
