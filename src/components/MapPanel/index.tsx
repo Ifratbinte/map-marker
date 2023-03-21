ea411fb00527c2ca7c0a63b4778acfc3fe887555
Ifrat Binte <ifratbinte@gmail.com>
2023-03-21 10:17:33 +0600

    feat(map) : add dragable marker

--- src/components/MapPanel/index.tsx
+++ src/components/MapPanel/index.tsx
@@ -1,12 +1,35 @@
 import "mapbox-gl/dist/mapbox-gl.css";
-import { useState } from "react";
-import Map, { FullscreenControl, GeolocateControl, Marker, NavigationControl } from "react-map-gl";
+import { useCallback, useState } from "react";
+import Map, { FullscreenControl, GeolocateControl, LngLat, Marker, NavigationControl, MarkerDragEvent } from "react-map-gl";
 
 const Index = () => {
   const [lng, setLng] = useState(54.37585762735543);
   const [lat, setLat] = useState(24.45677614934833);
-  return (
 
+   // onDrag
+  const [marker, setMarker] = useState({
+    latitude: 40,
+    longitude: -100,
+  });
+  const [events, logEvents] = useState<Record<string, LngLat>>({});
+
+  const onMarkerDragStart = useCallback((event: MarkerDragEvent) => {
+    logEvents(_events => ({..._events, onDragStart: event.lngLat}));
+  }, []);
+
+  const onMarkerDrag = useCallback((event: MarkerDragEvent) => {
+    logEvents(_events => ({..._events, onDrag: event.lngLat}));
+
+    setMarker({
+      longitude: event.lngLat.lng,
+      latitude: event.lngLat.lat
+    });
+  }, []);
+
+  const onMarkerDragEnd = useCallback((event: MarkerDragEvent) => {
+    logEvents(_events => ({..._events, onDragEnd: event.lngLat}));
+  }, []);
+  return (
     <Map
       mapboxAccessToken="pk.eyJ1IjoiaWZyYXRrYXppIiwiYSI6ImNsZmdkMTFocjE1Y20zdG56cXRncnR5cG0ifQ.avhOkXsuHDWB3vonRBGlng"
       style={{
@@ -18,7 +41,14 @@ const Index = () => {
       initialViewState={{ longitude: lng, latitude: lat }}
       mapStyle="mapbox://styles/mapbox/streets-v9"
     >
-      <Marker longitude={lng} latitude={lat} />
+      <Marker 
+        longitude={lng} 
+        latitude={lat} 
+        draggable 
+        onDragStart={onMarkerDragStart} 
+        onDrag={onMarkerDrag} 
+        onDragEnd={onMarkerDragEnd} 
+      />
       <NavigationControl position="top-right" />
       <GeolocateControl />
       <FullscreenControl position="top-right" />
